dfbb9675a24c6748b639f07e702a0f6ae9e58255
alqusi1992 <35297889+alqusi1992@users.noreply.github.com>
2021-09-13 01:32:16 +0200

    Fixing some issues

--- week2/hackyourweather/src/component/DataProfile.js
+++ week2/hackyourweather/src/component/DataProfile.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 function DataProfile({ weather }) {
+  const cityName = weather.city.name;
+  const countryName = weather.city.country;
+  const minTemp = weather.list[0].main.temp_min;
+  const maxTemp = weather.list[0].main.temp_max;
+  const weatherDesc = weather.list[0].weather[0].description;
+  const weatherMain = weather.list[0].weather[0].main;
+
   return (
     <div>
       <div className="list">
         <p>
-          {weather.country} {weather.name}
+          {cityName} {countryName}
         </p>
-        <p>Population: {weather.population}</p>
-        <p>Timezone : {weather.timezone}</p>
-        <p>Sunset: {weather.sunset}</p>
-        <p>Sunrise:{weather.sunrise}</p>
+        <p>MaxTemp : {maxTemp}</p>
+        <p>MinTemp: {minTemp}</p>
+        <p>Description: {weatherDesc}</p>
+        <p>WeatherMain:{weatherMain}</p>
       </div>
     </div>
   );
