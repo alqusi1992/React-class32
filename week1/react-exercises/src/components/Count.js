40a4ef4881e94541b33bdb90c0a230a9b84d9b14
alqusi1992 <35297889+alqusi1992@users.noreply.github.com>
2021-09-14 21:24:32 +0200

    Delete Count.js

--- week1/react-exercises/src/components/Count.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Count({ count }) {
-  const feedback = count > 10 ? "It's higher than 10!" : "Keep counting...";
-
-  return <div>{feedback}</div>;
-}
-
-export default Count;
